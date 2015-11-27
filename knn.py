e7843b2a9552141ea0bef313fcd6c75233dd21ad
Zhiya Zuo <zhiyazuo@gmail.com>
2015-11-27 14:05:42 -0600

    first commit

---
+++ knn.py
@@ -0,0 +1,48 @@
+def knn(data, k=3):
+
+    import numpy as np
+    from pyspark import SparkContext
+    from utils import eucdist, vote, find_neighbours
+
+    sc = SparkContext()
+
+    # Read file
+    img = sc.textFile(data)
+    imgRDD = img.map(lambda s: [int(t) for t in s.split()])
+
+    # Adds the index
+    RDDind = imgRDD.zipWithIndex()
+
+    # Switches positions of index and data
+    indRDD = RDDind.map(lambda (data,index):(index,data))
+
+    # Organizes into index,class,features
+    indClassFeat = indRDD.map(lambda (index,data): (index,data[-1],data[:-1]))
+
+    # Index, Class; Index, Feature
+    indClass = indClassFeat.map(lambda (index, class, features) : (index, class))
+    indFeat = indClassFeat.map(lambda (index, class, features) : (index, features))
+
+    # Creates all pairs of points
+    # Filter out the self-self pairs 
+    pairs = indClassFeat.cartesian(indClassFeat).filter(lambda (x1, x2): x1[0] != x2[0])
+
+    #Applies euclidean distance function to all pairs
+    imgED = pairs.map(eucdist)
+
+    # Creates key value pair where key is index1
+    KVpair = imgED.map(lambda x: (x[0],x[1:]))
+
+    # Group distances for each key
+    distRDD = KVpair.groupByKey().mapValues(list)
+
+    # Find k nearest neighbor for each key
+    # for each k, returns a list of tuples; each tuple (neighbour_index, neighbour_label)
+    sortedDistRDD = distRDD.map(lambda (idx, arr) : (idx, find_neighbours(arr)))
+
+    # Predict -- Majority Voting
+    predictionRDD = sortedDistRDD.map(lambda (idx, knns) : (idx, vote(knns)))
+
+if __name__ == '__main__':
+    knn('./test.txt')
+
---
+++ utils.py
@@ -0,0 +1,57 @@
+def vote(knns, weighted=False):
+    '''
+        Return predictions by voting
+        Default: Equal weights for all neighbours
+    '''
+
+    # binary class
+    pred_dict = {0: 0., 1:0.}
+    for n_i in knns:
+        this_label = int(n_i[1])
+
+        if weighted:
+            # weight inversely proportional to distance
+            distance = float(n_i[-1])
+            pred_dict[this_label] += 1/distance 
+
+        else:
+            pred_dict[this_label] += 1
+
+    # return the majority class label
+    return sorted(pred_dict, key=pred_dict.get)[-1]
+
+def find_neighbours(dist_array, k=1):
+    '''
+        Find the closest neighbours and 
+        return their indices, class labels, and distances (for weighted voting)
+    '''
+
+    import numpy as np
+    ind_dist_dict = {}
+    info_dict = {}
+    for item in dist_array:
+        ind_dist_dict[item[0]] = item[-1]
+        info_dict[item[0]] = item[1:]
+    top_k_indices = sorted(ind_dist_dict, key=ind_dist_dict.get)[:k]
+    
+    # return near neighbours along with their labels
+    k_neighbors = [(idx, info_dict[idx][1], info_dict[idx][-1]) for idx in top_k_indices]
+
+    return k_neighbors
+
+def eucdist(p):
+
+    '''
+        Euclidean distance function, takes in a pair of points
+    '''
+
+    import numpy as np
+    p1, p2 = p
+    #Unpacks first point into index,class,features
+    i1,c1,f1=p1
+    #Unpacks second point into index,class,features
+    i2,c2,f2=p2
+    #Initializes distance
+    dist = np.linalg.norm(np.asarray(f1) - np.asarray(f2))
+    #Returns index1,index2,class1,class2,euclidean distance
+    return (i1,i2,c1,c2,dist)
