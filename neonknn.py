11f66dbdfc8a1776290aa14c4c38d322fe8413e2
Meeshanthini Dogan <mvdogan@users.noreply.github.com>
2015-12-03 11:06:38 -0600

    Update neonknn.py

--- neonknn.py
+++ neonknn.py
@@ -1,13 +1,5 @@
 import numpy as np
 
-img = np.loadtxt("/home/mvijayen/BDA/test.txt")
-index = np.array(range(len(img)))
-index = index.reshape(len(index),1)
-features = img[:,0:-1]
-cls=img[:,[-1]]
-indClassFeat = np.concatenate((index,cls,features),axis=1)
-distance(indClassFeat)
-
 def cartesian (array1):
     from sklearn.utils.extmath import cartesian
     pairs = cartesian(array1, array1)
@@ -24,3 +16,11 @@ def distance (array1,norm=2):
     c2 = a2[:,[1]]
     dist = np.linalg.norm(np.asarray(f1) - np.asarray(f2), norm)
     return(i1,i2,c1,c2,dist)
+
+img = np.loadtxt("/Users/mvijayen/BDA/test.txt")
+index = np.array(range(len(img)))
+index = index.reshape(len(index),1)
+features = img[:,0:-1]
+cls=img[:,[-1]]
+indClassFeat = np.concatenate((index,cls,features),axis=1)
+cartesian(indClassFeat)
