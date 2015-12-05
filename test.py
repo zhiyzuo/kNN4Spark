34298f50a81756b6d3b8f1ebead094d6057c9549
Zhiya Zuo <zhiyazuo@gmail.com>
2015-12-05 15:20:45 -0600

    modify import functions

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import numpy as np
 from knn import KNN
 from pyspark import SparkConf, SparkContext
-from utils import get_distance, vote, find_neighbours, get_confusion_matrix, get_image_rdd
+from utils import cdist, vote, get_confusion_matrix, get_image_rdd
 
 sc = SparkContext()
 
@@ -10,5 +10,6 @@ sc = SparkContext()
 # Each element in x and y is (SubGroupKey, iterableResults)
 # in which iterableResults are (PixelKey, features/labels)
 x, y = get_image_rdd(sc, n_groups=5, start=0, end=10)
+x_, y_ = get_image_rdd(sc, start=10, end=11)
 
 sc.stop()
